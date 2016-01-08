3b87392f1a06742eda14deee164589dee1ba9360
John James Jacoby <johnjamesjacoby@me.com>
2016-01-07 23:21:52 -0600

    Bump class version & asset numbers.

--- includes/class-wp-term-family.php
+++ includes/class-wp-term-family.php
@@ -22,12 +22,12 @@ final class WP_Term_Family extends WP_Term_Meta_UI {
 	/**
 	 * @var string Plugin version
 	 */
-	public $version = '0.1.1';
+	public $version = '0.2.0';
 
 	/**
 	 * @var string Database version
 	 */
-	public $db_version = 201511230003;
+	public $db_version = 201601070001;
 
 	/**
 	 * @var string Database version
